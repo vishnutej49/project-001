8c7dee38320b75445695d8ba0635aab992704fa2
Vishnu <vishnutej49@gmail.com>
2025-01-27 11:51:57 +0530

    Security

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Hello there!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
-        </a>
       </header>
     </div>
   );
